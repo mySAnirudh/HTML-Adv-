e05550806efcd2c4bc7c666149eb8f3cd82ceed9
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-11-26 23:32:18 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-console.log(" This is Asynchronous operation 1")
-
-console.log(" This is Asynchronous operation 2")
-
-setTimeout(()=>{
-    
-console.log(" This is Asynchronous operation 3")
-
-},2000)
-console.log(" This is Asynchronous operation 4")
-
-console.log(" This is Asynchronous operation 5")
-
+const h1=require('h1').writeFileSync;
+h1("hellotc.txt","avf")
