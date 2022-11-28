06e097327536f161b0939be13a95c9e185335ff9
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-11-28 23:59:13 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
-// Here simple way to create file 
-
-const fs=require('fs'); // fs (file system ): It's js node module it's called file system
-
-fs.writeFileSync('apple.txt','this is file creating'); 
-
-// run in terminal :node .\index.js
-// THen auto create fiel with name apple.txt
+const h1=require('h1').writeFileSync;
+h1("hellotc.txt","avf")
